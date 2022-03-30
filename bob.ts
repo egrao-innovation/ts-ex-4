b885c30575a8e74adc029d39e784c73ec3df4836
egrao-innovation <em.grao@innovation.es>
2022-03-30 12:03:02 +0200

    finished

--- bob.ts
+++ bob.ts
@@ -1,3 +1,19 @@
 export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+  var response: string = 'Whatever.';
+  message = (message as string).trim();
+
+  if((message as string) === ''){
+    response = 'Fine. Be that way!'
+  }
+  else if(message == (message as string).toUpperCase() && (message as string).endsWith('?') && (message as string).match(/[a-zA-Z]/g)){
+    response = 'Calm down, I know what I\'m doing!'
+  }
+  else if((message as string) == (message as string).toUpperCase() && (message as string).match(/[a-zA-Z]/g)){
+    response = 'Whoa, chill out!';
+  }
+  else if((message as string).endsWith('?')){
+    response = 'Sure.'
+  }
+
+  return response;
 }
